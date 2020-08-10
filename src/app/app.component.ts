e19ec0b83e4cb0efb1fd0bf0f137cf2efc6bdb0e
codeconcept <contact@codeconcept.fr>
2020-08-10 13:41:38 +0200

    Show success message + toggle preview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,6 +22,10 @@ export class AppComponent implements OnDestroy {
   fileUploadSub: Subscription;
   blobSub: Subscription;
   pleaseWait = false;
+  message: string;
+  pictureProcessed = false;
+  isPreviewVisible = true;
+  togglePreviewMessage = 'prévisualisation activée';
 
   constructor(private imageService: ImageService) {}
 
@@ -37,6 +41,7 @@ export class AppComponent implements OnDestroy {
   }
 
   public handleImage(webcamImage: WebcamImage): void {
+    this.pictureProcessed = false;
     console.info('received webcam image', webcamImage);
     this.webcamImage = webcamImage;
     this.blobSub = this.dataURItoBlob(webcamImage.imageAsBase64).subscribe(
@@ -82,12 +87,26 @@ export class AppComponent implements OnDestroy {
       (imageData: any[]) => {
         console.log('imageData', imageData);
         this.pleaseWait = false;
+        this.message = 'image sauvegardée';
+        this.pictureProcessed = true;
+        this.showMessage({ duration: 2000, message: 'image sauvegardée'})
       },
       (err) => {
         console.error('AppComponent | upload() | err', err);
         this.pleaseWait = false;
       }
     );
+  }
+
+  showMessage(options) {
+    setTimeout( () => {
+      this.message = '';
+    }, options.duration);
+  }
+
+  togglePreviewVisibility() {
+    this.isPreviewVisible = !this.isPreviewVisible;
+    this.togglePreviewMessage = this.isPreviewVisible ? 'prévisualisation activée' : 'prévisualisation désactivée'
   }
 
   ngOnDestroy() {
