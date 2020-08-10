c53936d7996f76f998ed7b9ef1ed7cc70545d4de
codeconcept <contact@codeconcept.fr>
2020-08-10 14:32:21 +0200

    Form for creating new member + ng2-date-picker date picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import {WebcamModule} from 'ngx-webcam';
+import { WebcamModule } from 'ngx-webcam';
+import { DpDatePickerModule } from 'ng2-date-picker';
 
 import { AppComponent } from './app.component';
+import { MemberFormComponent } from './member-form/member-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, MemberFormComponent],
   imports: [
     BrowserModule,
     WebcamModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    DpDatePickerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/member-form/member-form.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { IDatePickerConfig } from 'ng2-date-picker';
+
+@Component({
+  selector: 'app-member-form',
+  templateUrl: './member-form.component.html',
+  styleUrls: ['./member-form.component.css'],
+})
+export class MemberFormComponent implements OnInit {
+  createMember: FormGroup;
+  datePickerConfig: IDatePickerConfig = { locale: 'fr', showMultipleYearsNavigation: true };
+  @Input() imageName;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.createMember = this.fb.group({
+      name: ['', Validators.required],
+      firstName: ['', Validators.required],
+      dob: [''],
+      imageName: this.imageName
+    });
+  }
+
+  create() {
+    console.log(this.createMember.value);
+  }
+}
